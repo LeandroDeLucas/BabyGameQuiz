2ad322507048dc6c46bdc2bb34cac80575c16cae
Daniel Roque <2006roque@gmail.com>
2017-08-30 11:26:03 -0300

    add label to show visible ants

--- src/scripts/ant_game.gd
+++ src/scripts/ant_game.gd
@@ -10,13 +10,14 @@ onready var newantstimer = get_node("NewAntsTimer")
 func _ready():
 	antinstance = preload("res://scenes/ant.tscn")
 	newantstimer.connect("timeout",self,"add_new_ants")
-	get_node("SecondPath").add_child(antinstance.instance())
-	get_node("FirstPath").add_child(antinstance.instance())
-	GLOBAL.set_inc_visible_ants(2)
-
+	add_new_ants()
+	set_fixed_process(true)
 
 func add_new_ants():
 	if(GLOBAL.get_visible_ants() < visibleAntsLimit):
 		GLOBAL.set_inc_visible_ants(2)
 		get_node("SecondPath").add_child(antinstance.instance())
 		get_node("FirstPath").add_child(antinstance.instance())
+
+func _fixed_process(delta):
+	get_node("AntAliveLabel").set_text(str(GLOBAL.get_visible_ants()))
